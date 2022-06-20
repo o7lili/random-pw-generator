4d7ae7712e7694cbb000a518a89c9bab67735b6a
lili <o7lilip@gmail.com>
2022-06-19 20:11:52 -0400

    started generatePassword() function for all true, all but special characters, and upper/lower case only conditionals

--- Develop/script.js
+++ Develop/script.js
@@ -18,6 +18,63 @@ function writePassword() {
 
 }
 
+//generate string pw for writePassword() function and return
+function generatePassword() {
+  let password = '';
+  //prompt for password length
+  var passwordLength = prompt("How many chararacters would you like your password to contain?");
+  // conditionals by confirm functions on length of pw (8-128 characters)
+  if(passwordLength >=8 && passwordLength <= 128) {
+    var upCase = confirm("Would you like uppercase letters in your password?");
+    if(upCase == true) {
+      var numberPick = confirm("Would you like numbers in your password?");
+      if(numberPick == true) {
+        var specialPick = confirm("Would you like special characters in your password?");
+        if(specialPick == true) {
+          // pw includes upper, lower, numbers, and special characters
+          var allTrue = upperCase.concat(lowerCase, numberSet, specialSet);
+            // for loop executes amount of times equal to the defined password length to generate randomly chosen characters from previously defined array
+            for(var i = 0; i < passwordLength; i++) {
+              characterNext = allTrue[Math.floor(Math.random() * Math.floor(allTrue.length))];
+              password = password.toString()+characterNext.toString();
+              console.log(password);
+            }
+        }
+        else {
+          // password includes upper, lower, and numbers (NOT special characters)
+          var upLowNumTrue = upperCase.concat(lowerCase, numberSet);
+          for(var i = 0; i < passwordLength; i++) {
+            characterNext = upLowNumTrue[Math.floor(Math.random() * Math.floor(upLowNumTrue.length))];
+            password = password.toString()+characterNext.toString();
+            console.log(password);
+          }
+        }
+      }
+      else {
+        var specialPick = confirm("Would you like special characters in your password?");
+        if(specialPick == true) {
+          // password includes upper, lower, and special characters (NOT number)
+          var upLowSpecTrue = upperCase.concat(lowerCase, specialSet);
+          for(var i = 0; i < passwordLength; i++) {
+            characterNext = upLowSpecTrue[Math.floor(Math.random() * Math.floor(upLowSpecTrue.length))];
+            password = password.toString()+characterNext.toString();
+            console.log(password);
+          }
+        }
+        else {
+          // password includes upper and lower (NOT numbers OR special characters)
+          var upLowTrue = upperCase.concat(lowerCase);
+          for(var i = 0; i < passwordLength; i++) {
+            characterNext = upLowTrue[Math.floor(Math.random() * Math.floor(upLowTrue.length))];
+            password = password.toString()+characterNext.toString();
+            console.log(password);
+          }
+        }
+      }
+    }
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
